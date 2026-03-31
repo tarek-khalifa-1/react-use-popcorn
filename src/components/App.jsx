9b8dbc3ddd23c82442955b7d3b629f5121a9eaaa
tarek-khalifa-1 <user.tarek.dev@gmail.com>
2026-03-31 05:38:00 +0200

    fix imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import Main from "./Main/Main";
 import Logo from "./Logo/Logo";
 import Search from "./Search/Search";
 import Box from "./Box/Box";
-import Loader from "./Loader/Loader";
 import MovieList from "./MovieList/MovieList";
 import WatchedSummary from "./WatchedSummary/WatchedSummary";
 import WatchedMoviesList from "./WatchedMoviesList/WatchedMoviesList";
